ab98b7ad65e1f2101448f52f5e10e47aa8596a13
Mateus Toledo <mateusmtoledo1@gmail.com>
2022-08-26 17:04:35 -0300

    Add axios

---
+++ frontend/src/api.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+});
+
+api.interceptors.request.use((config) => {
+  config.headers.Authorization = 'Bearer ' + localStorage.getItem('token');
+  return config;
+});
+
+export default api;
