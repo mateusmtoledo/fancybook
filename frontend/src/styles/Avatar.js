de3936b5dc1fa785a91fe28f53a783af070978ba
Mateus Toledo <mateusmtoledo1@gmail.com>
2022-09-09 15:39:18 -0300

    Set default attributes to Avatar

--- frontend/src/styles/Avatar.js
+++ frontend/src/styles/Avatar.js
@@ -1,8 +1,24 @@
 import styled from "styled-components";
 
-const Avatar = styled.img`
+const StyledAvatar = styled.img`
   border-radius: 100%;
   cursor: pointer;
 `;
+
+function Avatar({ src, alt, width, height }) {
+  return (
+    <StyledAvatar
+      src={src}
+      alt={alt}
+      onError={(event) => {
+        if(event.target.src !== 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png') {
+          event.target.src = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png';
+        }
+      }}
+      width={width || '32px'}
+      height={height || '32px'}
+    />
+  );
+}
 
 export default Avatar;
