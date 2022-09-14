8758be2e2a83ccff1d1afb88a53bbad8f31a4066
Mateus Toledo <mateusmtoledo1@gmail.com>
2022-09-13 23:38:41 -0300

    Add page query param to user posts route

--- backend/routes/userPosts.js
+++ backend/routes/userPosts.js
@@ -4,14 +4,20 @@ const Post = require('../models/Post');
 const router = express.Router({ mergeParams: true });
 
 router.get('/', (req, res, next) => {
+  const resultsPerPage = 8;
+  const page = Number(req.query.page) >= 0 ? Number(req.query.page) : 0;
   const { userId } = req.params;
-  Post.find({ author: userId }, (err, posts) => {
-    if (err) {
-      next(err);
-      return;
-    }
-    res.json({ posts });
-  });
+
+  Post.find({ author: userId })
+    .limit(resultsPerPage)
+    .skip(resultsPerPage * page)
+    .exec((err, posts) => {
+      if (err) {
+        next(err);
+        return;
+      }
+      res.json({ posts });
+    });
 });
 
 module.exports = router;
