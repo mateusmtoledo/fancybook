49f8c19448d680db4d8931033bbad9090ca5c652
Mateus Toledo <mateusmtoledo1@gmail.com>
2022-09-15 00:30:13 -0300

    Fix search results border

--- frontend/src/components/SearchResult.js
+++ frontend/src/components/SearchResult.js
@@ -3,28 +3,31 @@ import styled from "styled-components";
 import Avatar from "../styles/Avatar";
 
 const StyledSearchResult = styled.div`
-  display: flex;
-  gap: 8px;
-  align-items: center;
   border-bottom: 1px solid var(--color-gray-dark);
   padding: 12px 0;
 
   :last-child {
     border-bottom: none;
   }
+
+  a {
+    display: flex;
+    gap: 8px;
+    align-items: center;
+  }
 `;
 
 function SearchResult({ user }) {
   return (
-    <Link to={`/user/${user._id}`}>
-      <StyledSearchResult>
+    <StyledSearchResult>
+      <Link to={`/user/${user._id}`}>
         <Avatar
           src={user.avatar}
           alt={`${user.firstName}'s avatar`}
         />
         <p>{user.fullName}</p>
-      </StyledSearchResult>
-    </Link>
+      </Link>
+    </StyledSearchResult>
   );
 }
 
