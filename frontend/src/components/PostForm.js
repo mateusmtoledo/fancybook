a134351db736d34ea4d63ad4cd6588b863f05376
Mateus Toledo <mateusmtoledo1@gmail.com>
2022-09-16 15:25:23 -0300

    Implement simple validation for post text input

--- frontend/src/components/PostForm.js
+++ frontend/src/components/PostForm.js
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import styled from "styled-components";
 import { UserContext } from "../contexts/UserContext";
 import Avatar from "../styles/Avatar";
@@ -7,12 +7,23 @@ import SEND_ICON from '../img/send.svg';
 import api from "../adapters/api";
 import Loading from "./Loading";
 
+const Error = styled.p`
+  color: red;
+  font-size: 0.8rem;
+  /* position: absolute;
+  bottom: -12px; */
+`;
+
 const StyledPostForm = styled(Card)`
   position: relative;
   .post-text {
     display: flex;
     gap: 16px;
     align-items: center;
+  }
+  .user-input {
+    flex: 1;
+    position: relative;
   }
   .actions {
     display: flex;
@@ -24,23 +35,24 @@ const StyledPostForm = styled(Card)`
     gap: 16px;
   }
   textarea {
-    box-sizing: content-box;
     font-family: 'Roboto', sans-serif;
     padding: 8px;
     font-size: 1rem;
-    line-height: 19px;
     border-radius: 16px;
     background-color: var(--color-white);
     border: none;
     width: 100%;
     color: var(--color-gray-dark);
     resize: none;
-  }
-  textarea:focus-visible {
-    outline: none;
-  }
-  textarea::placeholder {
-    color: var(--color-gray-light);
+    &:focus {
+      outline: none;
+    }
+    &.invalid {
+      outline: 2px solid red;
+    }
+    &&::placeholder {
+      color: var(--color-gray-light);
+    }
   }
   button {
     background: none;
@@ -55,28 +67,40 @@ const StyledPostForm = styled(Card)`
 
 function PostForm({ refreshPosts }) {
   const { user } = useContext(UserContext);
-  const [text, setText] = useState('');
   const [loading, setLoading] = useState(false);
-
+  
+  const [text, setText] = useState('');
   function handleChange(event) {
     const element = event.target;
     setText(element.value);
     element.style.height = "auto";
-    element.style.height = (element.scrollHeight) - 16 + 'px';
+    element.style.height = element.scrollHeight + 'px';
   }
 
+  const [error, setError] = useState(null);
   async function submitPost(event) {
     event.preventDefault();
+    if (text.length < 3) {
+      setError('Post must have at least 3 characters');
+      return;
+    }
     setLoading(true);
     try {
       await api.post('/posts', { text });
+      setText('');
+      setError(null);
       refreshPosts();
     } catch (err) {
-      // TODO implement error handling
-      console.log(err);
+      setError('Something went wrong');
     }
     setLoading(false);
   }
+  
+  useEffect(() => {
+    if(error === 'Post must have at least 3 characters' && text.length >= 3) {
+      setError(null);
+    }
+  }, [text, error]);
 
   return (
     <StyledPostForm>
@@ -94,13 +118,19 @@ function PostForm({ refreshPosts }) {
             width="36px"
             height="36px"
           />
-          <textarea
-            value={text}
-            onChange={handleChange}
-            placeholder="Share your thoughts"
-            aria-label="Post text"
-            rows="1"
-          />
+          <div className="user-input">
+            <textarea
+              value={text}
+              onChange={handleChange}
+              placeholder="Share your thoughts"
+              aria-label="Post text"
+              rows="1"
+              className={error ? 'invalid' : null}
+            />
+            {error
+            ? <Error>{error}</Error>
+            : null}
+          </div>
         </div>
         <hr />
         <div className="actions">
