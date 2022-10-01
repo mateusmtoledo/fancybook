f1b2c2dd7dc3162b7c5dc59e87f420e092e2397c
Mateus Toledo <mateusmtoledo1@gmail.com>
2022-10-01 19:58:51 -0300

    Fix flickering issue by moving font imports into a css file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import GlobalFonts from './fonts/fonts';
 import GlobalStyles from './styles/GlobalStyles';
+import './fonts/fonts.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <GlobalStyles />
-    <GlobalFonts />
     <App />
   </React.StrictMode>
 );
