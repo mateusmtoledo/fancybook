7fbbafc09ab7a6c39c8c16f945e6c9486681d423
Mateus Toledo <mateusmtoledo1@gmail.com>
2022-10-05 22:23:08 -0300

    Improve date management

--- frontend/src/adapters/dateFormatter.js
+++ frontend/src/adapters/dateFormatter.js
@@ -1,20 +1,14 @@
 export function getDateString(date) {
-  const dateDifference = new Date() - new Date(date);
-  const yearsPassed = dateDifference / 31536000000;
-  if (yearsPassed >= 1)
-    return `${Math.floor(yearsPassed)} years ago`;
-  const monthsPassed = dateDifference / 2592000000;
-  if (monthsPassed >= 1)
-    return `${Math.floor(monthsPassed)} months ago`;
-  const daysPassed = dateDifference / 86400000;
-  if (daysPassed >= 1)
-    return `${Math.floor(daysPassed)} days ago`;
-  const hoursPassed = dateDifference / 3600000;
-  if (hoursPassed >= 1)
-    return `${Math.floor(hoursPassed)} hours ago`;
-  const minutesPassed = dateDifference / 60000;
-  if (minutesPassed >= 1)
-    return `${Math.floor(minutesPassed)} minutes ago`;
-  const secondsPassed = dateDifference / 1000;
-  return `${Math.floor(secondsPassed)} seconds ago`;
+  let dateNumber = (new Date() - new Date(date)) / 31536000000;
+  const dateDistances = [12, 30, 24, 60, 60, 1000];
+  const dateNames = ['year', 'month', 'day', 'hour', 'minute', 'second'];
+  for (let i = 0; i < dateDistances.length; i += 1) {
+    if (dateNumber >= 1 ||  i === dateDistances.length - 1) {
+      dateNumber = Math.floor(dateNumber);
+      return dateNumber === 1
+        ? `1 ${dateNames[i]} ago`
+        : `${dateNumber} ${dateNames[i]}s ago`;
+    }
+    dateNumber *= dateDistances[i];
+  }
 }
