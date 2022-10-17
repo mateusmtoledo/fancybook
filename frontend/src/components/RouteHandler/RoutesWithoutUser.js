20cc321a59cf2637484abbadfef8a70bb370da14
Mateus Toledo <mateusmtoledo1@gmail.com>
2022-10-17 18:35:45 -0300

    Fix router using wrong sign up component

--- frontend/src/components/RouteHandler/RoutesWithoutUser.js
+++ frontend/src/components/RouteHandler/RoutesWithoutUser.js
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Login from "../../pages/Login";
 import GoogleAuth from "../GoogleAuth";
-import SignUp from "../SignUpForm";
+import SignUp from "../../pages/SignUp";
 
 function RoutesWithoutUser() {
   return (
