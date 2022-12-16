f6f4a1d853ce5d1ac53d4544410cf89f7caa0bb4
Mateus Toledo <mateusmtoledo1@gmail.com>
2022-12-15 21:57:04 -0300

    Fix development setup

--- frontend/src/adapters/api.js
+++ frontend/src/adapters/api.js
@@ -1,9 +1,6 @@
 import axios from 'axios';
 
-const baseURL =
-  process.env.NODE_ENV === 'production'
-    ? process.env.REACT_APP_PROD_API_URL
-    : process.env.REACT_APP_API_URL || 'http://localhost:3001/api';
+const baseURL = process.env.REACT_APP_API_URL || 'http://localhost:3001/api';
 
 const api = axios.create({
   withCredentials: true,
